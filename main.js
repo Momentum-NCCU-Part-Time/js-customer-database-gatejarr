1f7daa8293aed7981897c61a7ea21db70b55b184
gatejarr <gatejarr@gmail.com>
2023-10-20 15:25:20 -0400

    prettier run

--- main.js
+++ main.js
@@ -4,6 +4,7 @@
 function customerDirectoryCard(customer) {
     return <div class="firstName"> $</div>
 }
+
 // Sample code from class
 let database = customers.map((customer) => customerDirectoryCard(customer))
 document.querySelector(.container).innerHTML = database;
