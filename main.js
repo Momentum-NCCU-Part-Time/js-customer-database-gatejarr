61c7f88477fbf7ab4b03db741607946455a1e57e
gatejarr <gatejarr@gmail.com>
2023-10-20 15:04:55 -0400

    added some basic code from class notes

---
+++ main.js
@@ -0,0 +1,9 @@
+
+
+//  Sample code from class
+function customerDirectoryCard(customer) {
+    return <div class="firstName"> $</div>
+}
+// Sample code from class
+let database = customers.map((customer) => customerDirectoryCard(customer))
+document.querySelector(.container).innerHTML = database;
