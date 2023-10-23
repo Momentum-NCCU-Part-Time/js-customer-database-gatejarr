6d9035f8391b6256e409f692ae0842e85d97ac70
Jarrod <gatejarr@gmail.com>
2023-10-22 23:17:30 -0400

    trying to pull info from one JS file to another

--- main.js
+++ main.js
@@ -1,11 +1,14 @@
 //  Is this necessay?
-//  import {customers} from '/customers.js';
+  import {customers} from customers.js;
 
-//  Sample code from class
+/*  Sample code from class
 function customerDirectoryCard(customer) {
     return <div class="firstName"> $</div>
-}
+} */
 
-// Sample code from class
+/* Sample code from class
 let database = customers.map((customer) => customerDirectoryCard(customer))
-document.querySelector('.container').innerHTML = database;
+document.querySelector('.container').innerHTML = database; */
+
+const firstName = customers.name.first;
+console.log(firstName);
