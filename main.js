c02c0ce001f6491ccb43bef581b7e9176fe39c11
Jarrod <gatejarr@gmail.com>
2023-10-24 21:12:43 -0400

    made some styling changes; just need to fix email so it's clickable

--- main.js
+++ main.js
@@ -1,26 +1,45 @@
 let customerContainer = document.querySelector(".container");
+
 function customerDirectoryCard(customer) {
   let customerCard = document.createElement("div");
+
   let customerPic = document.createElement("img");
-  customerPic.src = `${customer.picture.large}`;
+  customerPic.src = customer.picture.large;
+
   let customerName = document.createElement("h3");
-  customerName.innerText = `${customer.name.first} ${customer.name.last}`;
-  let customerEmail = document.createElement("mailto");
-  customerEmail.innerText = `${customer.email}`;
+  customerName.innerText =
+    customer.name.first.charAt(0).toUpperCase() +
+    customer.name.first.slice(1) +
+    " " +
+    customer.name.last.charAt(0).toUpperCase() +
+    customer.name.last.slice(1);
+
+  let customerEmail = document.createElement("a");
+  customerEmail.innerText = customer.email;
+
   let addressNumName = document.createElement("p");
-  addressNumName.innerText = `${customer.location.street.number} ${customer.location.street.name}`;
+  addressNumName.innerText =
+    customer.location.street.number + " " + customer.location.street.name;
+  addressNumName.classList.add("address");
+
   let addressCityStateZip = document.createElement("p");
-  addressCityStateZip.innerText = `${customer.location.city}, ${nameToAbbr(
-    customer.location.state
-  )} ${customer.location.postcode}`;
+  addressCityStateZip.innerText =
+    customer.location.city +
+    ", " +
+    nameToAbbr(customer.location.state) +
+    " " +
+    customer.location.postcode;
+
   let dateOfBirth = document.createElement("p");
-  dateOfBirth.innerText = `DOB: ${moment(customer.dob.date).format(
-    "MMM D, YYYY"
-  )}`;
+  dateOfBirth.innerText =
+    "DOB: " + moment(customer.dob.date).format("MMM D, YYYY");
+  dateOfBirth.classList.add("date");
+
   let customerSince = document.createElement("p");
-  customerSince.innerText = `Customer Since: ${moment(
-    customer.registered.date
-  ).format("MMM D, YYYY")}`;
+  customerSince.innerText =
+    "Customer Since: " + moment(customer.registered.date).format("MMM D, YYYY");
+
+  customerContainer.appendChild(customerCard);
   customerCard.appendChild(customerPic);
   customerCard.appendChild(customerName);
   customerCard.appendChild(customerEmail);
@@ -28,6 +47,5 @@ function customerDirectoryCard(customer) {
   customerCard.appendChild(addressCityStateZip);
   customerCard.appendChild(dateOfBirth);
   customerCard.appendChild(customerSince);
-  customerContainer.appendChild(customerCard);
 }
 let database = customers.map((customer) => customerDirectoryCard(customer));
